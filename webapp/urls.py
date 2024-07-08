d79d81209795044619977940a3c2975a9c01d420
Adam <adam.nemmish213@gmail.com>
2024-07-09 03:22:52 +0600

    Выполнил 2 этап: Реализовал CRUD Реализовал Формы Дописал шаблон base Реализовал частичный шаблон

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
+from webapp.views import HomePageView, DetailPageView, CreatePageView, EditPageView, DeletePageView
 
 urlpatterns = [
-
+    path('', HomePageView.as_view(), name='home'),
+    path('detail/<int:pk>/', DetailPageView.as_view(), name='detail'),
+    path('create/', CreatePageView.as_view(), name='create'),
+    path('edit/<int:pk>/', EditPageView.as_view(), name='edit'),
+    path('delete/<int:pk>/', DeletePageView.as_view(), name='delete'),
 ]
--- webapp/views.py
+++ webapp/views.py
@@ -1,3 +1,65 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.generic import TemplateView, View
+from webapp.forms import TaskForm
 
-# Create your views here.
+from webapp.models import Task
+
+
+class HomePageView(TemplateView):
+    template_name = 'home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['task'] = Task.objects.all()
+        return context
+
+
+class DetailPageView(TemplateView):
+    template_name = 'detail.html'
+
+    def get_context_data(self, **kwargs):
+        task = get_object_or_404(Task, pk=self.kwargs['pk'])
+        context = super().get_context_data(**kwargs)
+        context['task'] = task
+        return context
+
+
+class CreatePageView(View):
+    def get(self, request, *args, **kwargs):
+        form = TaskForm()
+        return render(request, 'create.html', {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            return render(request, 'create.html', {'form': form})
+
+
+class EditPageView(View):
+    def get(self, request, *args, **kwargs):
+        task = get_object_or_404(Task, pk=self.kwargs['pk'])
+        form = TaskForm(instance=task)
+        return render(request, 'update.html', {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        task = get_object_or_404(Task, pk=self.kwargs['pk'])
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect('detail', pk=task.id)
+        else:
+            return render(request, 'update.html', {'form': form})
+
+
+class DeletePageView(View):
+    def get(self, request, *args, **kwargs):
+        task = get_object_or_404(Task, pk=self.kwargs['pk'])
+        return render(request, 'delete.html', {'task': task})
+
+    def post(self, request, *args, **kwargs):
+        task = get_object_or_404(Task, pk=self.kwargs['pk'])
+        task.delete()
+        return redirect('home')
