7c04946c60007a6bd4927f4bf8a5ca418460befb
Adam <adam.nemmish213@gmail.com>
2024-07-09 00:26:37 +0600

    Выполнил 1 этап: Создал модели Создал(применил) миграцию Зарегал в админке Добавил админа и пару записей Накатил дамп

--- webapp/admin.py
+++ webapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from webapp.models import *
 
-# Register your models here.
+admin.site.register(Type)
+admin.site.register(Status)
+admin.site.register(Task)
+
--- webapp/models.py
+++ webapp/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class Type(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Status(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    summary = models.CharField(max_length=50, verbose_name='Краткое описание')
+    description = models.TextField(max_length=100, verbose_name='Полное описание', null=True, blank=True)
+    status = models.ForeignKey('webapp.Status', verbose_name='Статус', on_delete=models.PROTECT)
+    type = models.ForeignKey('webapp.Type', on_delete=models.PROTECT, verbose_name='Тип')
+    created_date = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+    updated_date = models.DateTimeField(auto_now=True, verbose_name='Время обновления')
+
+    def __str__(self):
+        return self.summary
