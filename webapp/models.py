6bc10429ff58eaf567e45f0e57b67ba02251fbc2
Adam <adam.nemmish213@gmail.com>
2024-07-09 15:30:02 +0600

    Выполнил 3 этап: Реализовал связь многие ко многим Сдела вывод Типов красивый Накатил миграции(применил) Обновил дамп

--- webapp/models.py
+++ webapp/models.py
@@ -19,7 +19,7 @@ class Task(models.Model):
     summary = models.CharField(max_length=50, verbose_name='Краткое описание')
     description = models.TextField(max_length=100, verbose_name='Полное описание', null=True, blank=True)
     status = models.ForeignKey('webapp.Status', verbose_name='Статус', on_delete=models.PROTECT)
-    type = models.ForeignKey('webapp.Type', on_delete=models.PROTECT, verbose_name='Тип')
+    type = models.ManyToManyField('webapp.Type', verbose_name='Тип', related_name='task')
     created_date = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
     updated_date = models.DateTimeField(auto_now=True, verbose_name='Время обновления')
 
