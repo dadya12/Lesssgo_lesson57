440f6ea4ca041bedeac761610620fd50473972e0
Adam <adam.nemmish213@gmail.com>
2024-07-11 22:05:42 +0600

    Выполнил 1 этап: Добавил два валидатора простых Модальные формы уже были сделаны, и были задействованы во вью :)

--- webapp/models.py
+++ webapp/models.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ValidationError
 from django.db import models
 
 
@@ -15,9 +16,19 @@ class Status(models.Model):
         return self.name
 
 
+def summary_valid(value):
+    if len(value) < 10:
+        raise ValidationError('Не может быть меньше 10 символов')
+
+
+def description_valid(value):
+    if 'Project' in value.capitalize():
+        raise ValidationError('Нельзя такое писать :)')
+
+
 class Task(models.Model):
-    summary = models.CharField(max_length=50, verbose_name='Краткое описание')
-    description = models.TextField(max_length=100, verbose_name='Полное описание', null=True, blank=True)
+    summary = models.CharField(max_length=50, verbose_name='Краткое описание', validators=[summary_valid])
+    description = models.TextField(max_length=100, verbose_name='Полное описание', null=True, blank=True, validators=[description_valid])
     status = models.ForeignKey('webapp.Status', verbose_name='Статус', on_delete=models.PROTECT)
     type = models.ManyToManyField('webapp.Type', verbose_name='Тип', related_name='task')
     created_date = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
