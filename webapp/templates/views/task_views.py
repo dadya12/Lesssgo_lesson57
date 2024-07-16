8982077f855881278242c9cfa68cae9a248c5922
Adam <adam.nemmish213@gmail.com>
2024-07-16 22:55:38 +0600

    Выполнил 3 этап: Переписал представления для редактирования, удаления и просмотра задач на джанговские UpdateView, DeleteView и DetailView

---
+++ webapp/templates/views/task_views.py
@@ -0,0 +1,40 @@
+from webapp.models import Task, Project
+from webapp.forms import TaskForm
+from django.views.generic import DetailView, CreateView, UpdateView, DeleteView
+from django.shortcuts import get_object_or_404, redirect, reverse
+
+
+class TaskDetailView(DetailView):
+    model = Task
+    template_name = 'Task/task_detail.html'
+
+
+class TaskCreateView(CreateView):
+    model = Task
+    form_class = TaskForm
+    template_name = 'Task/task_create.html'
+
+    def form_valid(self, form):
+        projects = get_object_or_404(Project, pk=self.kwargs.get('pk'))
+        task = form.save(commit=False)
+        task.project = projects
+        task.save()
+        form.save_m2m()
+        return redirect('detail', pk=projects.pk)
+
+
+class TaskEditView(UpdateView):
+    model = Task
+    form_class = TaskForm
+    template_name = 'Task/task_update.html'
+
+    def get_success_url(self):
+        return reverse('task_detail', kwargs={'pk': self.object.pk})
+
+
+class TaskDeleteView(DeleteView):
+    model = Task
+    template_name = 'Task/task_delete.html'
+
+    def get_success_url(self):
+        return reverse('home')
