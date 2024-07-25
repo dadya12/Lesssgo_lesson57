944cfe4d3f0e5bfd820da6fbde46a9db750e2b37
Adam <adam.nemmish213@gmail.com>
2024-07-25 22:50:54 +0600

    Выполнил 1 задание: Добавил в форму валидатор, который проверяет, что хотя бы одно из полей (first_name, last_name) заполнено. Сделал поле email в форме обязательным для заполнения.

---
+++ accounts/forms.py
@@ -0,0 +1,25 @@
+from django.contrib.auth.models import User
+from django import forms
+
+
+class MyUserCreationForm(forms.ModelForm):
+    email = forms.EmailField(required=True)
+
+    def clean(self):
+        cleaned_data = super().clean()
+        first_name = cleaned_data.get("firs_name")
+        last_name = cleaned_data.get("last_name")
+        if not first_name and not last_name:
+            raise forms.ValidationError('Please fill out either the First Name or Last Name field!')
+        return cleaned_data
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password"])
+        if commit:
+            user.save()
+        return user
+
+    class Meta:
+        model = User
+        fields = ['username', 'password', 'first_name', 'last_name', 'email']
